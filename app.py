50426d6256d203e4bcb75fa29632a398f8cefd6f
Genet0609 <118865821+Genet0609@users.noreply.github.com>
2023-08-07 22:59:45 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -43,19 +43,30 @@ def handler_message(event):
 
 
     
+# 定義一個字典來存儲使用者的狀態，預設狀態為 0（未加入好友）
+user_status = {}
+
 @handler.add(FollowEvent)
 def handler_follow(event):
     welcome_message = """哈囉！你好哇！恭喜你已經成為了莉飄兒大人的部下！"""
+    user_id = event.source.user_id
     
-    line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=welcome_message))
+    # 若使用者狀態為 1（解除封鎖後再重新加入好友），則發送特定訊息
+    if user_status.get(user_id) == 1:
+        block_message = """哇哈哈！莉飄兒大人果然還是最好的，對吧？"""
+        line_bot_api.push_message(user_id, TextSendMessage(text=block_message))
+    else:
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=welcome_message))
     
+    # 將使用者狀態設置為 0（加入好友）
+    user_status[user_id] = 0
 
 @handler.add(UnfollowEvent)
 def handle_unfollow(event):
-    block_message = """哇哈哈！莉飄兒大人果然還是最好的，對吧？"""
-    line_bot_api.push_message(event.source.user_id, TextSendMessage(text=block_message))
+    user_id = event.source.user_id
+    # 將使用者狀態設置為 1（解除封鎖後再重新加入好友）
+    user_status[user_id] = 1
+
 
 
 if __name__ == '__main__':
