81c2d25f3d155c7a5dbfb97a2c7344a7866849ad
Genet0609 <118865821+Genet0609@users.noreply.github.com>
2023-08-07 13:34:09 +0800

    Add files via upload

---
+++ events/basic.py
@@ -0,0 +1,66 @@
+from line_bot_api import *
+
+def about_us_event(event):
+    
+    emoji = [
+
+        {
+            "index" : 0,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"002"
+        },
+        {
+            "index" : 1,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"039"
+        },
+
+        {
+            "index" : 2,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"052"
+        },
+
+        {
+            "index" : 3,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"012"
+        },
+
+        {
+            "index" : 4,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"053"
+        },
+
+        {
+            "index" : 5,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"002"
+        },
+
+        {
+            "index" : 6,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"031"
+        },
+
+        {
+            "index" : 7,
+            "productId": "5ac21c4e031a6752fb806d5b",
+            "emojiId" :"014"
+        }
+
+    ]
+
+    text_message = TextSendMessage(text="""$$$$$$$$ 
+哈囉！你好哇！恭喜你已經成為了莉飄兒大人的部下！""",emojis=emoji)
+    
+    sticker_message = StickerSendMessage(
+        package_id="11537",
+        sticker_id="52002748"
+    )
+    line_bot_api.reply_message(
+        event.reply_token,
+        [text_message,sticker_message]
+    )
