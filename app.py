f4a2d613fa82f210428b7302eb9487a8a7693243
Genet0609 <118865821+Genet0609@users.noreply.github.com>
2023-08-07 16:33:29 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -27,7 +27,7 @@ def handler_message(event):
 # 選單
 
     if message_text == '@使用說明':
-        about_us_event(event)
+        #about_us_event(event)
         Usage(event)
 
     if event.message_text == '@油價':
@@ -36,6 +36,20 @@ def handler_message(event):
             event.reply_token,
             TextSendMessage(text = content)
         )
+    
+@handler.add(FollowEvent)
+def handler_follow(event):
+    welcome_message = TextSendMessage(text="""哈囉！你好哇！
+恭喜你已經成為了莉飄兒大人的部下！""")
+    
+    line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=welcome_message))
+    
+
+@handler.add(UnfollowEvent)
+def handle_unfollow(event):
+    print(event)
 
 if __name__ == '__main__':
     app.run()
