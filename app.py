a8b3afe84cfd7e937e232c404ea2f2ce3618d0aa
Genet0609 <118865821+Genet0609@users.noreply.github.com>
2023-08-09 14:40:40 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -65,19 +65,24 @@ def handler_message(event):
         line_bot_api.push_message(uid,TextSendMessage(content))
         return 0"""
     
-    if re.match("想知道股價", msg):
-        stockNumber = msg[5:]
-        btn_msg = stock_reply_other(stockNumber)    
+    if event.message.text =='股價查詢':
+        line_bot_api.push_message(uid, TextSendMessage('請輸入#+股票代號....'))
+#股票查詢
+    if re.match("想知道股價[0-9]", msg):
+        stockNumber = msg[5:9]
+        btn_msg = stock_reply_other(stockNumber)
         line_bot_api.push_message(uid, btn_msg)
         return 0
-    #新增使用者關注股票到mongodb
-    if re.match('關注[0-9]{4}[<>][0-9]',msg):
-        stockNumber = msg[2:]
-        line_bot_api.push_message(uid, TextSendMessage("加入股票代號"+stockNumber))
-        content = write_my_stock(uid, user_name, stockNumber, msg[6:7], msg[7:])
-        line_bot_api.push_message(uid, TextSendMessage(content))
+
+#新增使用者關注的股票到mongodb
+    if re.match("關注[0-9]{4}[<>][0-9]",msg):#使用者新增股票至股票清單    
+        stockNumber = msg[2:6]
+        line_bot_api.push_message(uid, TextSendMessage('加入股票代號'+stockNumber))
+        content = write_my_stock(uid, user_name,stockNumber,msg[6:7],msg[7:])
+        
+    
     else:
-        content = write_my_stock(uid, user_name, stockNumber, "未設定", "未設定")
+        content = write_my_stock(uid, user_name, stockNumber, "未設定",'未設定')
         line_bot_api.push_message(uid, TextSendMessage(content))
         return 0
     
